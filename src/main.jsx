5913fc883530a3bb702a8a06d9e33314c6b37a57
IJ-Jany <146128361+IJ-Jany@users.noreply.github.com>
2024-01-31 00:30:24 +0600

    homepage

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import firebaseConfig from './config/FirebaseConfig.jsx'
-
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {store} from './store.js'
 import { Provider } from 'react-redux'
+import { store } from './store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-)
+     <App />
+   </Provider>,
+ )
--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit'
 
-export default configureStore({
-  reducer: {},
+import { configureStore } from '@reduxjs/toolkit'
+export const store = configureStore({
+  reducer:{},
 })
